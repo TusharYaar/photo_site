9bbd02b9365547ef03bd2b2cace8dcd2398fc618
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2026-03-24 00:01:22 +0530

    fix build issues

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [],
-	},
+  vite: {
+    plugins: [],
+  },
+  image: {
+    domains: ["cdn.sanity.io"],
+  },
 });
